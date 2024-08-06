3895379faa7ea6df3f469956d0d4110a1b5032a7
Chandu2828 <purnachand7569@gmail.com>
2024-08-06 18:09:05 +0530

    success

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -6,10 +6,10 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR::Please run this script with with root access"
+    echo "ERROR:: Please run this script with root access"
     exit 1
-#else
-#   echo "INFO::You are root user"
+# else
+#     echo "INFO:: You are root user"
 fi
 
 # it is our responsibility again to check installation is success or not
