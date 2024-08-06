d482b0526a2190f2debaaa80da37c8cd5d021994
Chandu2828 <purnachand7569@gmail.com>
2024-08-06 17:32:22 +0530

    success

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,10 @@
-! /bin/bash
-echo "Hello I am learning devops"
+#!/bin/bash
+
+echo "Hello World, I am learning DevOps with AWS"
+PERSON1 = Chandu
+PERSON2 = Purna 
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, how are you $PERSON1"
