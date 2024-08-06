2976236a277a9677344c7c0ba4a046b4953500fe
Chandu2828 <purnachand7569@gmail.com>
2024-08-06 18:58:18 +0530

    success

--- 09-functions.sh
+++ 09-functions.sh
@@ -12,9 +12,9 @@ N="\e[0m"
 
 # this function should validate the previous command and inform user it is success or failure
 VALIDATE(){
-     #$1 --> it will receive the argument1
-     if [ $1 -ne 0 ]
-     then
+    #$1 --> it will receive the argument1
+    if [ $1 -ne 0 ]
+    then
         echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
