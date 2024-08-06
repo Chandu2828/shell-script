ee575b8108d001a20b4ea489940c6bc1098cf503
Chandu2828 <purnachand7569@gmail.com>
2024-08-06 17:51:40 +0530

    success

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#check if number is greater than 10 or not
+# check if number is greater than 10 or not
 NUMBER=$1
 
-if [$NUMBER -gt 10]
+if [ $NUMBER -gt 10 ]
 then
     echo "$NUMBER is greater than 10"
 else 
